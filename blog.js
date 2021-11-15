b131548941f146cbc0ffa27bac34949822ad3135
Andre <foreandr@gmail.com>
2021-11-14 19:25:34 -0500

    blog link to new page

--- blog.js
+++ blog.js
@@ -1,25 +1,32 @@
-var titleOfPost;
-var contentsOfPost;
-var date;
-function process() {
+function process(titleOfPost, date, bloghtmlFile) {
+    /*
     titleOfPost = "title of Post"; // change title to whatever
     contentsOfPost = "Contents of Post";
     date = "nov 2 2222";
+    */
+
     document.getElementById("enterBlogPosts").innerHTML +=
         `<div> 
-        Title: ${titleOfPost}
+        <a class="${bloghtmlFile}" onclick="printFunction()">Title: ${titleOfPost}</a>
         </br>
         Date: ${date}
-        </br>
-        Contents: ${contentsOfPost}
-        </div>
-        </br>`;
+        </br> </br>`;
 
     // Add more Posts
+    console.log(bloghtmlFile);
 }
-
 function printFunction() {
-
+    console.log("skzjhlkjhadsf");
 }
+//<button class="link">Clicky</button>
+// First Blog Post test
+var titleOfPost1 = "This is me";
+var date1 = "Jan 1 2020";
+var linkToHtml = "./blog1.html";
 
-process();
+process(titleOfPost1, date1, linkToHtml);
+
+// Second Blog Post test
+var titleOfPost2 = "This is me2";
+var date2 = "Jan 2 2020";
+//process(titleOfPost2, date2);
--- projects.js
+++ projects.js
@@ -13,8 +13,8 @@ function addToTerminal(stringOfText) {
 }
 
 
-/* coder:
- * purpose:
+/* coder: andre
+ * purpose: take in a key, assign this key to a value, print to console
 
  */
 function myKeyPress(e) {
@@ -30,20 +30,47 @@ function myKeyPress(e) {
 }
 
 
-/* coder:
- * purpose:
+/* coder: andre
+ * purpose: 
+ * notes: I can't figure this out
  
  */
+let totalString = "";
+let forBackspace = "";
 document.body.addEventListener("keydown", function (event) {
-    term.write(String.fromCharCode(event.keyCode));
-    if (event.keyCode == 13) {
+    // console.log(event); GET FULL OBJECT
+
+
+    if (event.keyCode == 13) { // ENTER KEY
         term.write("\n");
+        totalString += "\n";
+
     }
-    if (event.keyCode == 8) {
+    if (event.keyCode == 8) { //BACKSPACE
+
         term.write("Backspace");
-            //console.log("backspace");
+        //totalString += ("/");
+        for (let i = 0; i < totalString.length - 1; i++) { // NOT SURE WHY MINUS 2
+            forBackspace += totalString[i];
+            //console.log("Being Added[i]" + forBackspace[i]);
+            //console.log("Total string[i]" + totalString[i]);
+        }
+
+        console.log("Total for backspace string " + forBackspace);
     }
-    if (event.keyCode == 46) {
+    if (event.keyCode == 46) { // DELETE
         term.write("delete");
+        totalString += "delete";
     }
+    if (event.keyCode == 189) { // DELETE
+        term.write("-");
+        totalString += "-";
+    }
+
+    else { // PRINT WHATEVER ELSE THERE IS
+        totalString += String.fromCharCode(event.keyCode);
+        term.write(String.fromCharCode(event.keyCode));
+    }
+
+    //console.log(totalString);
 });
