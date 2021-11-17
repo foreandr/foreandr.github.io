e188263a196d8eac74dfa368a92a9c445a5882da
Andre <foreandr@gmail.com>
2021-11-17 00:21:20 -0500

    making sur eeverything is commited

--- finance.js
+++ finance.js
@@ -1,26 +1,23 @@
+
+
 function printTitle() {
     var today = new Date();
     $gel("titleOfGraph").innerHTML = "<h4><u>Todays Date: " + today + "</u></h4>";
 }
 printTitle();
-
-var myVar2 = require('./papaparse') //to use local modules
-file = "C:\Users\Andre\Desktop\FOLDER\foreandr.github.io\csv\file.csv"
-
-
-/*
-Papa.parse(file, {
-    complete: function (results) {
-        console.log(results);
-    }
-});
- */ 
 function $gel(id) {
     return document.getElementById(id);
 }
 
 
+file_ = "C:\Users\Andre\Desktop\FOLDER\foreandr.github.io\txt\file.txt"
 
-
-
-//console.log("hello");
+/* DOESNT WORK 
+async function loadFile(file) {
+    let text = await file.text();
+    document.getElementById('output').textContent = text;
+}
+*/
+/* DOESNT WORK 
+loadFile(file_)
+*/
