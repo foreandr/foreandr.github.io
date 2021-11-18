2cd0ff94a8681248023131db14293a6fc2989223
Andre <foreandr@gmail.com>
2021-11-18 00:09:09 -0500

    TRIED NEW FILE STUFF

--- finance.js
+++ finance.js
@@ -1,23 +1,23 @@
 
+function $gel(id) {
+    return document.getElementById(id);
+}
 
 function printTitle() {
     var today = new Date();
     $gel("titleOfGraph").innerHTML = "<h4><u>Todays Date: " + today + "</u></h4>";
 }
 printTitle();
-function $gel(id) {
-    return document.getElementById(id);
-}
+ 
+// DOESNT WORK
+const fs = require('fs)');
+file_ = "C:/Users/Andre/Desktop/FOLDER/foreandr.github.io/txt/file.txt";
 
+fs.readFile(file_, 'utf-8', (err, data) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(data);
+});
 
-file_ = "C:\Users\Andre\Desktop\FOLDER\foreandr.github.io\txt\file.txt"
-
-/* DOESNT WORK 
-async function loadFile(file) {
-    let text = await file.text();
-    document.getElementById('output').textContent = text;
-}
-*/
-/* DOESNT WORK 
-loadFile(file_)
-*/
