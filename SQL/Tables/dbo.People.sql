be558fba4499d8879fe415c2334a8e20be052591
Andre <foreandr@gmail.com>
2021-11-19 15:47:53 -0500

    sql stuff

--- SQL/Tables/dbo.People.sql
+++ SQL/Tables/dbo.People.sql
@@ -9,3 +9,18 @@ CREATE TABLE [dbo].[People]
 GO
 ALTER TABLE [dbo].[People] ADD CONSTRAINT [PK__People__3214EC27A087DAEF] PRIMARY KEY CLUSTERED ([ID]) ON [PRIMARY]
 GO
+
+
+USE myDatabase;
+GO
+CREATE TABLE dbo.Blog
+(
+ID INT NOT NULL IDENTITY(1, 1),
+Title NVARCHAR (50),
+Author NVARCHAR (50), 
+uploadedDate DATE,
+contents NVARCHAR(8000),
+) 
+GO
+
+DROP TABLE dbo.Blog;
